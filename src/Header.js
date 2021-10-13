f9f05435763fb2f5bef03378797c15b29e876de1
anthonychan1211 <Anthony@Apples-MacBook-Pro.local>
2021-10-13 15:29:19 +0800

    first upload

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="container">
+            <h1 className="header">Lesson Record</h1>
+            <button className="btn-red">Delete Student</button>
+            <button className="btn-green">Add Student</button>
+            
+        </div>
+    )
+}
+
+export default Header
